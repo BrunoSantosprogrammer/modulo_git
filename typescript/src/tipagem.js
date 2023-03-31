e2c918414f384a19e421808ca70e1d6e759ce0dc
Bruno Santos <rapazcomumvl@hotmail.com>
2023-03-31 16:46:13 -0300

    exercicio_typeScript

---
+++ typescript/src/tipagem.js
@@ -0,0 +1,9 @@
+"use strict";
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+console.log(soma(20, 10));
+function nomes(nome) {
+    return `"Olá ${nome}"`;
+}
+console.log(nomes("Bruno"));
---
+++ typescript/src/tipagem.ts
@@ -0,0 +1,10 @@
+function soma(numero1: number, numero2: number): number{
+    return numero1 + numero2;
+}
+console.log(soma(20, 10))
+
+function nomes (nome: string): string {
+    return `"Olá ${nome}"`;
+}
+
+console.log(nomes("Bruno"))
