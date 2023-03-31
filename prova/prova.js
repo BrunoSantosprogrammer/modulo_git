beb75f142f5aa9d12d80ad11216616a34940c38c
BrunoSantos <111144170+BrunoSantosprogrammer@users.noreply.github.com>
2023-03-31 16:48:07 -0300

    Delete prova directory

--- prova/prova.js
+++
@@ -1,4 +0,0 @@
-let alunos = [{nome:'Bruno', nota: 7},{nome:'Evellyn', nota: 4},{nome:'Yara', nota: 6},{nome:'Bruna', nota: 2},{nome:'João', nota: 5},{nome:'Maria', nota: 10},{nome:'Bianca', nota: 2},{nome:'Gian', nota: 10},{nome:'Leandro', nota: 5}];
-const alunosAprovados = alunos.filter(aluno => aluno.nota >= 6)
-
-console.log(alunosAprovados)
